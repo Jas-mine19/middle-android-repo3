50cb7edbe0f09a4f4ccf421a762a7cdd65dce844
Jasmina <shonazmievajasmin@gmail.com>
2026-01-21 16:39:06 +0500

    done with task 3

--- app/src/main/java/ru/yandex/architectureproject/TaskViewModel.kt
+++ app/src/main/java/ru/yandex/architectureproject/TaskViewModel.kt
@@ -6,46 +6,74 @@ import kotlinx.coroutines.flow.StateFlow
 import ru.yandex.architectureproject.model.Task
 import ru.yandex.architectureproject.model.TaskAction
 import ru.yandex.architectureproject.model.TaskState
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-
-
-class TaskViewModel : ViewModel() {
+class TaskViewModel(
+    private val repository: TaskRepository = InMemoryTaskRepository(),
+    private val completeTaskUseCase: CompleteTaskUseCase =
+        CompleteTaskUseCase(repository)
+) : ViewModel() {
 
     private val _state = MutableStateFlow(TaskState())
     val state: StateFlow<TaskState> = _state
+
+    private val autoDeleteJobs = mutableMapOf<Int, Job>()
 
     fun reduce(action: TaskAction) {
         when (action) {
 
             is TaskAction.LoadTasks -> {
-                // пока ничего не делаем
+                viewModelScope.launch {
+                    _state.value = _state.value.copy(
+                        tasks = repository.getTasks()
+                    )
+                }
             }
 
             is TaskAction.AddTask -> {
-                val newTask = Task(
-                    id = (_state.value.tasks.maxOfOrNull { it.id } ?: 0) + 1,
-                    text = action.text,
-                    isDone = false
-                )
-                _state.value = _state.value.copy(
-                    tasks = _state.value.tasks + newTask
-                )
+                viewModelScope.launch {
+                    val task = Task(
+                        id = (_state.value.tasks.maxOfOrNull { it.id } ?: 0) + 1,
+                        text = action.text,
+                        isDone = false
+                    )
+                    repository.addTask(task)
+                    reduce(TaskAction.LoadTasks)
+                }
             }
 
             is TaskAction.UpdateTaskStatus -> {
-                _state.value = _state.value.copy(
-                    tasks = _state.value.tasks.map {
-                        if (it.id == action.taskId)
-                            it.copy(isDone = action.isDone)
-                        else it
+                viewModelScope.launch {
+                    val task = _state.value.tasks.first { it.id == action.taskId }
+                        .copy(isDone = action.isDone)
+
+                    repository.updateTask(task)
+
+                    if (action.isDone) {
+                        completeTaskUseCase.execute(
+                            scope = viewModelScope,
+                            taskId = task.id
+                        ) { job ->
+                            autoDeleteJobs[task.id] = job
+                        }
+                    } else {
+                        autoDeleteJobs[task.id]?.cancel()
+                        autoDeleteJobs.remove(task.id)
                     }
-                )
+
+                    reduce(TaskAction.LoadTasks)
+                }
             }
 
             is TaskAction.DeleteTask -> {
-                _state.value = _state.value.copy(
-                    tasks = _state.value.tasks.filterNot { it.id == action.taskId }
-                )
+                viewModelScope.launch {
+                    autoDeleteJobs[action.taskId]?.cancel()
+                    autoDeleteJobs.remove(action.taskId)
+                    repository.deleteTask(action.taskId)
+                    reduce(TaskAction.LoadTasks)
+                }
             }
         }
     }
